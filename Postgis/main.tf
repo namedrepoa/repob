f31800b874b4798daddd134d760da28630c0ba1b
namedrepoa <63254484+namedrepoa@users.noreply.github.com>
2020-04-06 19:36:36 +0200

    Create main.tf

---
+++ Postgis/main.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "5345ererteertertetertete"
+    storage_account_name = "adasdasdasder4rgrtyh6rgg"
+    container_name       = "gen-tfstates"
+    key                  = "curasan.pre.terraform.tfstate"
+  }
+}
